5703a3bb0da7271091978b01d0f337a2c70e66e0
iazigit <31214223+iazigit@users.noreply.github.com>
2022-01-09 01:52:39 +0100

    init commit

---
+++ scripts/start.js
@@ -0,0 +1,25 @@
+async function main() {
+    const [owner, somebodyElse] = await hre.ethers.getSigners();
+    const keyboardsContractFactory = await hre.ethers.getContractFactory("Keyboards");
+    const keyboardsContract = await keyboardsContractFactory.deploy();
+    await keyboardsContract.deployed();
+
+    console.log("Contract deployed to:", keyboardsContract.address);
+
+    let keyboards = await keyboardsContract.getKeyboards();
+    console.log("We got the keyboards!", keyboards);
+
+    const keyboardTxn1 = await keyboardsContract.create(0, true, "sepia");
+    const tipTxn = await keyboardsContract.connect(somebodyElse).tip(0, { value: hre.ethers.utils.parseEther("1") })
+    const tipTxnReceipt = await tipTxn.wait();
+    console.log(tipTxnReceipt.events);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
